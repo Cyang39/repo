20cf66dbdbbbfc2fc5300dac01f2c8eb939ff301
Cyang39 <huqinyang123@qq.com>
2023-07-05 16:26:10 +0800

    add ioctl 带整型参数实例

---
+++ driver/day03/ioctl2/head.h
@@ -0,0 +1,19 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+typedef struct {
+	unsigned int MODER;
+	unsigned int OTYPER;
+	unsigned int OSPEEDR;
+
+	unsigned int PUPDR;
+	unsigned int IDR;
+	unsigned int ODR;
+} gpio_t;
+#define PHY_LED1_ADDR 0X50006000
+#define PHY_LED2_ADDR 0X50007000
+#define PHY_LED3_ADDR 0X50006000
+#define PHY_RCC_ADDR 0X50000A28
+
+#define LED_ON _IOW('l', 1, int) //开灯
+#define LED_OFF _IOW('l', 0, int) //关灯
+#endif
---
+++ driver/day03/ioctl2/test.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include "head.h"
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	char buf[128] = { 0 };
+	int fd = open("/dev/mychrdev0", O_RDWR);
+	if (fd < 0) {
+		printf("打开设备文件失败\n");
+		exit(-1);
+	}
+	while (1) {
+		//从终端读取
+
+		printf("请选择要实现的功能\n");
+		printf("0(关灯) 1(开灯)>");
+
+		scanf("%d", &a);
+
+		printf("请输入要控制的灯\n");
+		printf("1(LED1) 2(LED2) 3(LED3)>");
+		scanf("%d", &b);
+		if (a == 1) {
+			ioctl(fd, LED_ON, b);
+		} else if (a == 0) {
+			ioctl(fd, LED_OFF, b);
+		}
+	}
+	close(fd);
+
+	return 0;
+}
