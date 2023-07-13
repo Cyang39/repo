b48e3058c12649703308ec4eaa72d77e4e627a48
Cyang39 <huqinyang123@qq.com>
2023-07-13 15:17:53 +0800

    📄️add：day10/platform/pdrv_table.c

---
+++ driver/day10/platform/pdrv_table.c
@@ -0,0 +1,56 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/mod_devicetable.h>
+
+struct resource *res;
+unsigned int irqno;
+
+//probe函数，匹配设备成功执行
+int pdrv_probe(struct platform_device *pdev)
+{
+  printk("%s:%s:%d\n", __FILE__, __func__, __LINE__);
+  //获取设备信息
+  res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+  if (res == NULL) {
+    printk("获取资源失败\n");
+    return -ENXIO;
+  }
+  printk("获取资源信息成功 %llx\n", res->start);
+  irqno = platform_get_irq(pdev, 0);
+  if (irqno < 0) {
+    printk("获取中断资源失败\n");
+    return irqno;
+  }
+  printk("中断类型资源为%d\n", irqno);
+  return 0;
+}
+
+//remove 设备和驱动分离时执行
+int pdrv_remove(struct platform_device *pdev)
+{
+  printk("%s:%s:%d\n", __FILE__, __func__, __LINE__);
+  return 0;
+}
+
+//构建名字表
+struct platform_device_id idtable[] = {
+  { "aaaaa", 0 },
+  { "bbbbb", 1 },
+  { "ccccc", 2 },
+  { "ddddd", 3 },
+  {}, //防止数组越界
+};
+
+struct platform_driver pdrv={
+  .probe=pdrv_probe,
+  .remove=pdrv_remove,
+  .driver={
+    .name="aaaaa", 
+  },
+  .id_table=idtable,//设置名字表匹配
+};
+
+//一键注册宏
+module_platform_driver(pdrv);
+MODULE_LICENSE("GPL");
